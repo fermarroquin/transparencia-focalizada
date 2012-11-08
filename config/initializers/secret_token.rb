436b003f4a848327eb0f8bc3e7ea8e93b499c93e
ismaelrd <ismael@gobiernoabierto.gob.sv>
2012-11-08 12:14:24 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tf::Application.config.secret_token = 'b4ae455d797f240f5b4e81cb42ff5b27e8f6b7e3d1f6a79837838bc222c4ff3175a96ed35827ff111cf4876299b1c8cb9a87b12d76536d63db17013f194b65bc'
