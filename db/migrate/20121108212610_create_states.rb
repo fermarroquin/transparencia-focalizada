8e298bef6ffaecef34daba2ea2536b4fc36f2ac7
ismaelrd <ismael@gobiernoabierto.gob.sv>
2012-11-08 16:34:13 -0600

    carga automática para departamentos (geografía)

--- db/migrate/20121108212610_create_states.rb
+++ db/migrate/20121108212610_create_states.rb
@@ -1,9 +1,36 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class CreateStates < ActiveRecord::Migration
-  def change
+  def up
+    
     create_table :states do |t|
       t.string :name
-
       t.timestamps
     end
+    
+    [
+      { :id => 1, :name => 'Ahuachapán' },
+      { :id => 2, :name => 'Cabañas' },
+      { :id => 3, :name => 'Chalatenango' },
+      { :id => 4, :name => 'Cuscatlán' },
+      { :id => 5, :name => 'La Libertad' },
+      { :id => 6, :name => 'La Paz' },
+      { :id => 7, :name => 'La Unión' },
+      { :id => 8, :name => 'Morazán' },
+      { :id => 9, :name => 'San Miguel' },
+      { :id => 10, :name => 'San Salvador' },
+      { :id => 11, :name => 'San Vicente' },
+      { :id => 12, :name => 'Santa Ana' },
+      { :id => 13, :name => 'Sonsonate' },
+      { :id => 14, :name => 'Usulután' }
+    ].each do |data|
+      State.create(data)
+    end
+        
+  end
+  
+  def down
+    drop_table :states
   end
 end
